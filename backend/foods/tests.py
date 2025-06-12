70aa7b8c167c049bd6af6367b0d04096cd1e1162
Julia Tleuberdina <ysgaldina@mail.ru>
2025-06-12 16:43:52 +0500

    Add tests

---
+++ backend/foods/tests.py
@@ -0,0 +1,28 @@
+from http import HTTPStatus
+
+from foods import models
+from django.test import Client, TestCase
+
+class RecipeAPITestCase(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка рецептов."""
+        response = self.guest_client.get('/api/recipes/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_recipe_creation(self):
+        """Проверка создания рецепта."""
+        data = {
+          "ingredients": [
+            {"name": "авокадо", "amount": 1}
+          ],
+          "tags": [1],
+          "name": "Test",
+          "text": "приготовление авокадо",
+          "cooking_time": "20",
+          "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAAAACXBIWXMAAA7EAAAOxAGVKw4>
+        }
+
+        response = self.guest_client.post('/api/recipes/', data=data)

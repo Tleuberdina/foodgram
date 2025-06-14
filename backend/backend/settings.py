261a67c9d0ff7cee4a47917c077988b97230ebd1
Julia Tleuberdina <ysgaldina@mail.ru>
2025-06-14 17:45:56 +0500

    str recipes list23

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,6 +139,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / '/backend_static/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "frontend/src/image",
+]
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

eb33a1d5df09ea791f8e535d8987dc782a4e6b1d
Julia Tleuberdina <ysgaldina@mail.ru>
2025-06-14 17:16:33 +0500

    str recipes list22

--- backend/foods/tests.py
+++ backend/foods/tests.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.test import TestCase
 from foods import models
 from rest_framework.test import APIClient
 

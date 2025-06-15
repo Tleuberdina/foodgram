616008c55023fcc51100f363554da6457c602214
Julia Tleuberdina <ysgaldina@mail.ru>
2025-06-15 15:24:33 +0500

    correction3

--- backend/foods/tests.py
+++ backend/foods/tests.py
@@ -57,7 +57,7 @@ class RecipeAPITestCase(TestCase):
             '/api/recipes/',
             data=data,
             format='json',
-             follow=True,
+            follow=True,
             HTTP_AUTHORIZATION=f'Token {self.token.key}'
         )
         self.assertEqual(response.status_code, HTTPStatus.CREATED)

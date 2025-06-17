c65e6aa6f6579bab205f957ffd73296e588b72f5
Julia Tleuberdina <ysgaldina@mail.ru>
2025-06-17 18:33:04 +0500

    correction17

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.templatetags.static import static
 
 
 class MyUser(AbstractUser):
@@ -19,4 +18,3 @@ class MyUser(AbstractUser):
 
     def __str__(self):
         return self.email
-

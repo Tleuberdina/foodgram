c9d3aae3bd6da003757f2d6ddcdfdc7c34f126c3
Julia Tleuberdina <ysgaldina@mail.ru>
2025-06-26 12:17:33 +0500

    two

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 
 from .models import ExtendedUser
 
 
+class EmailAdminAuthForm(AuthenticationForm):
+    username = forms.EmailField(
+        label="Email", 
+        widget=forms.EmailInput(attrs={'autofocus': True})
+    )
+
+    def clean_username(self):
+        return self.cleaned_data['username']
+
+
 class ExtendedUserAdmin(UserAdmin):
+    login_form = EmailAdminAuthForm
     list_display = ('email', 'first_name', 'last_name')
     ordering = ('email',)
 
